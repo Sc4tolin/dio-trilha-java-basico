3b37299460baed90d99a67fc01ea7e450b647970
Weid Scatolin <spotifyweid@gmail.com>
2024-07-18 16:40:19 -0300

    metodos

---
+++ tipos-variaveis/src/Metodos.java
@@ -0,0 +1,26 @@
+public class Metodos {
+
+    //qualquer ação é considerada metodo // atributos na maioria das vezes são variaves
+
+    //deve ser nomeado com verbo 
+    // seguir o padrão camel cacase (todas as letras minusculas, com exceção da primeira letra da segunda palavra )
+
+    // o metodo precisa ter uma unica responsabilidade EX : ou calcular ou imprimir 
+
+    // os metodos precisam estar dentro de uma classe 
+
+    // qual a principal proposta do metodo ? se perguntar constantemente até compreender a real finalidade do mesmo
+
+    // qual o tipo de retorno esperado após executar o metodo ? você deve analisar se o metodo será responsavel por retornar algum valor ou não 
+
+    // Quais os parametros necessarios para execução do metodo ? Os metodos as vezes vão precisar de argumentos como criterios para execução 
+
+    // O metodo possui o risco de apresentar alguma exceção ? Exceções são comuns na execução de metodos, as vezes é necessario prever e tratar a possivel existencia de uma exceção
+
+    // qual a visibilidade do metodo ? Será necessario que o metodo seja visivel a toda aplicação, somente em mesmo pacotes, através de herança ou somente a nivel da propria classe ?
+
+    // public ou private
+    
+
+    
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -12,18 +12,20 @@
 
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        final double VALOR_DE_PI = 3.14;
         String meuNome = "weid";
 
         double salarioMinimo = 2500;
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+        
 
         int numero = 5;
         numero = 10;
         System.out.println(numero);
 
-        final double VALOR_DE_PI = 3.14;
+       
         
 
 
